3d428ff139fa2d217962e9b4d67267e06c162764
Jason Komsa <jasonkomsa@hotmail.com>
2023-06-12 17:21:11 +0200

    Initial quadTree branch

---
+++ js/quadTree.js
@@ -0,0 +1,26 @@
+class Quadtree {
+	constructor( a, b, c ) {
+		this._a = a
+		this._ = 
+		this._ = 
+	}
+	// GETTERS
+	get a() { return this._a }
+	get () { return this._ }
+	get () { return this._ }
+	// SETTERS
+	set a( a ) {
+		this._a = a
+	}
+	set (  ) {
+		this._ = 
+	}
+	set (  ) {
+		this._ = 
+	}
+	// METHODS
+	method( arg ) {
+		//console.info(`method(${arg})`)
+    	// code
+	}
+}
